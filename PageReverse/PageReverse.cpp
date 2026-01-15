60ae0bc35b79f510d7c0389eb7b6cd716020a6da
shhwang <shhwang@fourthlogic.co.kr>
2026-01-15 11:19:27 +0900

    Modify Page Reverse Example's View Information Text

--- PageReverse/PageReverse.cpp
+++ PageReverse/PageReverse.cpp
@@ -126,13 +126,13 @@ int main()
 			//                 얼라인 -> 폰트 이름 -> 폰트 알파값(불투명도) -> 면 알파값 (불투명도) -> 폰트 두께 -> 폰트 이텔릭
 			// Parameter order: layer -> reference coordinate Figure object -> string -> font color -> Area color -> font size -> actual size -> angle ->
 			//                  Align -> Font Name -> Font Alpha Value (Opaqueness) -> Cotton Alpha Value (Opaqueness) -> Font Thickness -> Font Italic
-		if(IsFail(res = layerAfterSrc.DrawTextCanvas(&flpDrawingPoint, L"Source Image After Shuffle", YELLOW, BLACK, 20)))
+		if(IsFail(res = layerAfterSrc.DrawTextCanvas(&flpDrawingPoint, L"Source Image After Reverse", YELLOW, BLACK, 20)))
 		{
 			ErrorPrint(res, "Failed to draw text\n");
 			break;
 		}
 
-		if(IsFail(res = layerBeforeSrc.DrawTextCanvas(&flpDrawingPoint, L"Source Image Before Shuffle", YELLOW, BLACK, 20)))
+		if(IsFail(res = layerBeforeSrc.DrawTextCanvas(&flpDrawingPoint, L"Source Image Before Reverse", YELLOW, BLACK, 20)))
 		{
 			ErrorPrint(res, "Failed to draw text\n");
 			break;
