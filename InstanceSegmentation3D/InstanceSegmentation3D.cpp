9ca3d95b20b8eb48c7924123025e64d5e76adfca
sbno <sbno@fourthlogic.co.kr>
2026-01-23 18:42:28 +0900

    Fix Instance Segmentation 3D Build Error

--- InstanceSegmentation3D/InstanceSegmentation3D.cpp
+++ InstanceSegmentation3D/InstanceSegmentation3D.cpp
@@ -373,9 +373,11 @@ int main()
 		instanceSegmentation3DDL.SetInferenceResultImage(fliResultBoxContourImage);
 		// 추론 결과 옵션 설정 // Set the inference result options
 		// Figure 옵션 설정 // Set the option of figures
-		CInstanceSegmentation3DDL::EInferenceResultItemSettings eFigureOption = (CInstanceSegmentation3DDL::EInferenceResultItemSettings)(CInstanceSegmentation3DDL::EInferenceResultItemSettings_ClassNum | CInstanceSegmentation3DDL::EInferenceResultItemSettings_ClassName | CInstanceSegmentation3DDL::EInferenceResultItemSettings_Objectness | CInstanceSegmentation3DDL::EInferenceResultItemSettings_BoundaryRect | CInstanceSegmentation3DDL::EInferenceResultItemSettings_Region);
-		instanceSegmentation3DDL.SetInferenceResultItemSettings(eFigureOption);
+		CInstanceSegmentation3DDL::EInferenceResultItemSettings eFigureOption = (CInstanceSegmentation3DDL::EInferenceResultItemSettings)(CInstanceSegmentation3DDL::EInferenceResultItemSettings_ClassNum | CInstanceSegmentation3DDL::EInferenceResultItemSettings_ClassName | CInstanceSegmentation3DDL::EInferenceResultItemSettings_Objectness);
+		CInstanceSegmentation3DDL::EInferenceResultRegionFigureType eFigureType = (CInstanceSegmentation3DDL::EInferenceResultRegionFigureType)(CInstanceSegmentation3DDL::EInferenceResultRegionFigureType_Region | CInstanceSegmentation3DDL::EInferenceResultRegionFigureType_BoundaryRectangle);
 
+		instanceSegmentation3DDL.SetInferenceResultItemSettings(eFigureOption);
+		instanceSegmentation3DDL.SetInferenceResultRegionFigureType(eFigureType);
 		// 추론 시의 Camera Calibrator 설정 // Set the camera calibrator to inference
 		instanceSegmentation3DDL.SetInferenceCameraCalibrator(flpFocalLength, flpPrincipalPoint, 1.f, flaDistortionCoefficient, EDirectionType_Decrement);
 
