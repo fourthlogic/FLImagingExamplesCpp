2591de88f7b081db337295fdbef54c40e2b5b303
shhwang <shhwang@fourthlogic.co.kr>
2026-08-20 18:51:54 +0900

    Add New comparison image in Focus Measurement Example

--- SumModifiedLaplacian/SumModifiedLaplacian.cpp
+++ SumModifiedLaplacian/SumModifiedLaplacian.cpp
@@ -13,10 +13,12 @@ int main()
 	// 이미지 객체 선언 // Declare image object
 	CFLImage fliSrcImage1;
 	CFLImage fliSrcImage2;
+	CFLImage fliSrcImage3;
 
 	// 이미지 뷰 선언 // Declare image view
 	CGUIViewImageWrap viewImageSrc1;
 	CGUIViewImageWrap viewImageSrc2;
+	CGUIViewImageWrap viewImageSrc3;
 
 	do
 	{
@@ -29,6 +31,12 @@ int main()
 		}
 
 		if(IsFail(res = fliSrcImage2.Load(L"../../ExampleImages/FocusMeasurement/Focus2.flif")))
+		{
+			ErrorPrint(res, "Failed to load the image file.\n");
+			break;
+		}
+
+		if(IsFail(res = fliSrcImage3.Load(L"../../ExampleImages/FocusMeasurement/Focus3.flif")))
 		{
 			ErrorPrint(res, "Failed to load the image file.\n");
 			break;
@@ -47,6 +55,12 @@ int main()
 			break;
 		}
 
+		if(IsFail(res = viewImageSrc3.Create(1424, 0, 1936, 612)))
+		{
+			ErrorPrint(res, "Failed to create the image view.\n");
+			break;
+		}
+
 		// Source 이미지 뷰에 이미지를 디스플레이 // Display the image in the source image view
 		if(IsFail(res = viewImageSrc1.SetImagePtr(&fliSrcImage1)))
 		{
@@ -60,15 +74,33 @@ int main()
 			break;
 		}
 
-		// 두 이미지 뷰의 시점을 동기화 한다 // Synchronize the viewpoints of the two image views
+		if(IsFail(res = viewImageSrc3.SetImagePtr(&fliSrcImage3)))
+		{
+			ErrorPrint(res, "Failed to set image object on the image view.\n");
+			break;
+		}
+
+		// 이미지 뷰의 시점을 동기화 한다 // Synchronize the viewpoints of the image views
 		if(IsFail(res = viewImageSrc1.SynchronizePointOfView(&viewImageSrc2)))
 		{
 			ErrorPrint(res, "Failed to synchronize view\n");
 			break;
 		}
 
-		// 두 이미지 뷰 윈도우의 위치를 맞춤 // Synchronize the positions of the two image view windows
+		if(IsFail(res = viewImageSrc1.SynchronizePointOfView(&viewImageSrc3)))
+		{
+			ErrorPrint(res, "Failed to synchronize view\n");
+			break;
+		}
+
+		// 이미지 뷰 윈도우의 위치를 맞춤 // Synchronize the positions of the image view windows
 		if(IsFail(res = viewImageSrc1.SynchronizeWindow(&viewImageSrc2)))
+		{
+			ErrorPrint(res, "Failed to synchronize window.\n");
+			break;
+		}
+
+		if(IsFail(res = viewImageSrc1.SynchronizeWindow(&viewImageSrc3)))
 		{
 			ErrorPrint(res, "Failed to synchronize window.\n");
 			break;
@@ -106,14 +138,29 @@ int main()
 		// 결과 점수 획득 // Get Result Score
 		double f64Score2 = sumModifiedLaplacian.GetResultScore();
 
+		// Source 이미지 3 설정 // Set the source3 image
+		sumModifiedLaplacian.SetSourceImage(fliSrcImage3);
+
+		// 앞서 설정된 파라미터 대로 알고리즘 수행 // Execute algorithm according to previously set parameters
+		if(IsFail(res = sumModifiedLaplacian.Execute()))
+		{
+			ErrorPrint(res, "Failed to execute Sum Modified Laplacian.");
+			break;
+		}
+
+		// 결과 점수 획득 // Get Result Score
+		double f64Score3 = sumModifiedLaplacian.GetResultScore();
+
 		// 화면에 출력하기 위해 Image View에서 레이어 0번을 얻어옴 // Obtain layer 0 number from image view for display
 		// 이 객체는 이미지 뷰에 속해있기 때문에 따로 해제할 필요가 없음 // This object belongs to an image view and does not need to be released separately
 		CGUIViewImageLayerWrap layerSrc1 = viewImageSrc1.GetLayer(0);
 		CGUIViewImageLayerWrap layerSrc2 = viewImageSrc2.GetLayer(0);
+		CGUIViewImageLayerWrap layerSrc3 = viewImageSrc3.GetLayer(0);
 
 		// 기존에 Layer에 그려진 도형들을 삭제 // Clear the figures drawn on the existing layer
 		layerSrc1.Clear();
 		layerSrc2.Clear();
+		layerSrc3.Clear();
 
 		// 점수를 디스플레이 한다. // Display score
 		// 아래 함수 DrawTextCanvas 는 Screen좌표를 기준으로 하는 String을 Drawing 한다. // The function DrawTextCanvas below draws a String based on the screen coordinates.
@@ -125,6 +172,7 @@ int main()
 
 		CFLString<wchar_t> flsLayer1 = CFLString<wchar_t>().Format(L"Score : %lf", f64Score1);
 		CFLString<wchar_t> flsLayer2 = CFLString<wchar_t>().Format(L"Score : %lf", f64Score2);
+		CFLString<wchar_t> flsLayer3 = CFLString<wchar_t>().Format(L"Score : %lf", f64Score3);
 
 		if(IsFail(res = layerSrc1.DrawTextCanvas(&CFLPoint<double>(0, 0), flsLayer1, YELLOW, BLACK, 20)))
 		{
@@ -138,12 +186,19 @@ int main()
 			break;
 		}
 
+		if(IsFail(res = layerSrc3.DrawTextCanvas(&CFLPoint<double>(0, 0), flsLayer3, YELLOW, BLACK, 20)))
+		{
+			ErrorPrint(res, "Failed to draw text\n");
+			break;
+		}
+
 		// 이미지 뷰를 갱신 합니다. // Update image view
 		viewImageSrc1.Invalidate(true);
 		viewImageSrc2.Invalidate(true);
+		viewImageSrc3.Invalidate(true);
 
 		// 이미지 뷰가 종료될 때 까지 기다림 // Wait for the image view to close
-		while(viewImageSrc1.IsAvailable() && viewImageSrc2.IsAvailable())
+		while(viewImageSrc1.IsAvailable() && viewImageSrc2.IsAvailable() && viewImageSrc3.IsAvailable())
 			CThreadUtilities::Sleep(1);
 	}
 	while(false);
