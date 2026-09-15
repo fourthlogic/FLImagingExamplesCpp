1dd11e2c3c7a0ccb2d38a587c89661288a058a65
ks <ks@fourthlogic.co.kr>
2026-09-15 14:09:42 +0900

    Add Device Laser Profile Sensor Mech-Eye example.

---
+++ DeviceLaserProfileSensorMechEye/DeviceLaserProfileSensorMechEye.cpp
@@ -0,0 +1,267 @@
+﻿#include <cstdio>
+
+#include <FLImaging.h>
+
+
+// 프로파일 센서에서 데이터 취득 이벤트를 받기 위해 CDeviceEventProfileBase 를 상속 받아서 구현
+// Inherit from CDeviceEventProfileBase to receive data acquisition events from the profile sensor.
+class CDeviceEventProfileEx : public CDeviceEventProfileBase
+{
+public:
+	// CDeviceEventProfileEx 생성자 // Constructor of CDeviceEventProfileEx
+	CDeviceEventProfileEx()
+	{
+		m_pView3D = nullptr;
+	}
+
+	// CDeviceEventProfileEx 소멸자 // Destructor of CDeviceEventProfileEx
+	virtual ~CDeviceEventProfileEx()
+	{
+	}
+
+	// 취득한 데이터를 표시할 3D 뷰를 설정하는 함수 // Sets the 3D view used to display the acquired data.
+	void SetView3D(CGUIView3DWrap* pView3D)
+	{
+		if(pView3D)
+			m_pView3D = pView3D;
+	}
+
+	// 프로파일 센서에서 데이터 취득 시 호출 되는 함수 // Callback function called when a data is acquired from the profile sensor.
+	virtual void OnAcquisition(const CDeviceProfileBase* pDeviceProfile)
+	{
+		do
+		{
+			if(!m_pView3D)
+				break;
+
+			// 3D 뷰의 유효성을 확인한다. // Check the validity of the 3D view.
+			if(!m_pView3D->IsAvailable())
+				break;
+
+			const CDeviceLaserProfileSensorMechEyeBase* pSensor = dynamic_cast<const CDeviceLaserProfileSensorMechEyeBase*>(pDeviceProfile);
+
+			if(!pSensor)
+				break;
+
+			// 데이터 객체 선언 // Declare a 3D data object.
+			CFL3DObject floData;
+
+			// 프로파일 센서에서 취득 한 데이터를 얻어온다. // Get the 3D data acquired from the profile sensor.
+			pSensor->GetAcquired3DData(floData);
+
+			// 3D 뷰의 업데이트를 막습니다. // Lock updates to the 3D view.
+			m_pView3D->LockUpdate();
+
+			// 3D 뷰의 유효성을 확인한다. // Check the validity of the 3D view.
+			if(!m_pView3D->IsAvailable())
+				break;
+
+			// 3D 뷰의 객체 개수를 얻어옵니다. // Get the number of objects in the 3D view.
+			int32_t i32ObjectCount = m_pView3D->GetObjectCount();
+
+			// 3D 뷰의 객체들을 모두 클리어합니다. // Clear all objects in the 3D view.
+			m_pView3D->ClearObjects();
+
+			// 3D 뷰의 유효성을 확인한다. // Check the validity of the 3D view.
+			if(!m_pView3D->IsAvailable())
+				break;
+
+			// 3D 뷰에 객체를 추가합니다. // Push objects to the 3D view.
+			m_pView3D->PushObject(floData);
+
+			// 3D 뷰의 유효성을 확인한다. // Check the validity of the 3D view.
+			if(!m_pView3D->IsAvailable())
+				break;
+
+			// 3D 뷰의 업데이트 막은 것을 해제합니다. // Unlock 3D view updates.
+			m_pView3D->UnlockUpdate();
+
+			// 3D 뷰의 스케일을 조정합니다. // Adjust the scale of the 3D view.
+			if(!i32ObjectCount)
+				m_pView3D->ZoomFit();
+		}
+		while(false);
+	}
+
+protected:
+	CGUIView3DWrap* m_pView3D;
+};
+
+
+int main()
+{
+	// You must call the following function once
+	// before using any features of the FLImaging(R) library
+	CLibraryUtilities::Initialize();
+
+	CResult drReturn = EResult_UnknownError;
+
+	// 3D 뷰 선언 // Declare a 3D view.
+	CGUIView3DWrap view3D;
+
+	// MechEye 프로파일 센서 선언 // Declare MechEye profile sensor.
+	CDeviceLaserProfileSensorMechEye_2_6_0 sensor;
+
+	do
+	{
+		wchar_t arrInput[4096] = { 0, };
+		bool bAutoDetect = false;
+		int32_t i32SelectDevice = 0;
+		CFLString<wchar_t> flsConnection = L"";
+
+		// 장치 찾기 방법을 선택합니다. // Select the detection method
+		while(true)
+		{
+			printf("1. Auto Detect\n");
+			printf("2. Manual\n");
+			printf("Select Detection Method: ");
+			fgetws(arrInput, 4096, stdin);
+
+			CFLString<wchar_t> flsInput(arrInput);
+			int32_t i32Select = flsInput.ToInt32();
+			bool bSelected = true;
+
+			switch(i32Select)
+			{
+			case 1:
+				bAutoDetect = true;
+				break;
+
+			case 2:
+				bAutoDetect = false;
+				break;
+
+			default:
+				bSelected = false;
+				break;
+			}
+
+			if(bSelected)
+				break;
+
+			printf("Incorrect input. Please select again.\n\n");
+		}
+
+		printf("\n");
+
+		if(bAutoDetect)
+		{
+			CFLArray<CFLString<wchar_t>> flarrSerialNumbers;
+
+			// 연결되어 있는 프로파일 센서의 시리얼 번호를 얻는다. // Get the serial number of the connected profile sensor.
+			drReturn = sensor.GetAutoDetectSerialNumbers(flarrSerialNumbers);
+
+			if(drReturn.IsFail() || !flarrSerialNumbers.GetCount())
+			{
+				drReturn = EResult_FailedToRead;
+				printf("Not Found Device.\n");
+				break;
+			}
+
+			// 연결 할 프로파일 센서를 선택합니다. // Select the profile sensor to connect to.
+			while(true)
+			{
+				for(int64_t i = 0; i < flarrSerialNumbers.GetCount(); ++i)
+				{
+					CFLString<wchar_t> flsElement;
+					flsElement.Format(L"%d. ", i + 1);
+
+					flsElement += flarrSerialNumbers.GetAt(i);
+					flsElement += L"\n";
+
+					wprintf_s(flsElement);
+				}
+
+				printf("Select Device: ");
+				fgetws(arrInput, 4096, stdin);
+
+				CFLString<wchar_t> flsInput(arrInput);
+				int32_t i32Select = flsInput.ToInt32() - 1;
+
+				if(i32Select < 0 || i32Select >= flarrSerialNumbers.GetCount())
+				{
+					printf("Incorrect input. Please select again.\n\n");
+					continue;
+				}
+
+				i32SelectDevice = i32Select;
+
+				break;
+			}
+		}
+		else
+		{
+			// 시리얼 번호를 입력 받습니다. // Enter the serial number.
+			printf("Input Serial Number: ");
+			fgetws(arrInput, 4096, stdin);
+
+			flsConnection = arrInput;
+			flsConnection.Replace(L"\n", L"");
+		}
+
+		// 이벤트를 받을 객체 선언 // Declare an event object.
+		CDeviceEventProfileEx eventProfile;
+
+		// 프로파일 센서에 이벤트 객체 설정 // Set the event object for the profile sensor.
+		sensor.RegisterDeviceEvent(&eventProfile);
+
+		if(bAutoDetect)
+			// 인덱스에 해당하는 프로파일 센서로 연결을 설정합니다. // Set the profile sensor specified by the index for connection.
+			drReturn = sensor.AutoDetect(i32SelectDevice);
+		else
+			// 시리얼 번호를 설정합니다. // Set the serial number.
+			sensor.SetSerialNumber(flsConnection);
+
+		// 프로파일 센서를 초기화 합니다. // Initialize the profile sensor.
+		drReturn = sensor.Initialize();
+
+		if(drReturn.IsFail())
+		{
+			printf("Failed to initialize the profile sensor.\n");
+			break;
+		}
+
+		// 3D 뷰 생성 // Create a 3D view.
+		if(view3D.Create(0, 0, 1000, 1000).IsFail())
+		{
+			drReturn = EResult_FailedToCreateObject;
+
+			printf("Failed to create the image view.\n");
+			break;
+		}
+
+		eventProfile.SetView3D(&view3D);
+
+		// 프로파일 센서를 Start 합니다. // Start acquisition.
+		drReturn = sensor.Start();
+
+		if(drReturn.IsFail())
+		{
+			printf("Failed to start the profile sensor.\n");
+			break;
+		}
+
+		// 프로파일 센서에 소프트웨어 트리거를 발생합니다. // Triggers the profile sensor using a software trigger.
+		drReturn = sensor.Trigger();
+
+		if(drReturn.IsFail())
+		{
+			printf("Failed to trigger the profile sensor.\n");
+			break;
+		}
+
+		// 3D 뷰가 종료될 때 까지 기다림 // Wait for the 3D view to close.
+		while(view3D.IsAvailable())
+			CThreadUtilities::Sleep(1);
+	}
+	while(false);
+
+	// 프로파일 센서의 초기화를 해제합니다. // Terminate the profile sensor.
+	sensor.Terminate();
+	sensor.ClearDeviceEvents();
+
+	if(drReturn.IsFail())
+		getchar();
+
+	return 0;
+}
